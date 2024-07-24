c6b4c8a445a715a88a221929b38afc2dcf749c4b
Jonas Deprund <jonasdeprund@gmail.com>
2024-07-24 18:58:34 +0200

    date picker wip

--- components/ui/SearchForm.tsx
+++ components/ui/SearchForm.tsx
@@ -1,10 +1,14 @@
 'use client';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useRouter } from 'next/router';
-import { useForm } from 'react-hook-form';
-import * as z from 'zod';
 
+import { zodResolver } from '@hookform/resolvers/zod'; // 2k (gzipped: 923)
+import { useRouter } from 'next/navigation'; // 11.3k (gzipped 2.8k)
+import { useForm } from 'react-hook-form'; // 19.9k (gzipped: 7.6k)
+import * as z from 'zod'; // 55.8k (gzipped: 13.5k)
 import { Button } from '@/components/ui/button';
+import { BedDoubleIcon, CalendarIcon } from 'lucide-react'; // 1.5k (gzipped: 859)
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
+import { format } from 'date-fns';
+
 import {
   Form,
   FormControl,
@@ -15,7 +19,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { BedDoubleIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 export const formSchema = z.object({
   location: z.string().min(2, 'Must 2 characters or more').max(50),
@@ -84,7 +88,39 @@ function searchForm() {
             control={form.control}
             name="dates"
             render={({ field }) => (
-              <FormItem className="flex flex-col"></FormItem>
+              <FormItem className="flex flex-col">
+                <FormLabel className="text-white">Dates</FormLabel>
+                <FormMessage />
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        id="date"
+                        name="dates"
+                        variant="outline"
+                        className={cn(
+                          'w-[300px] justify-start text-left font-normal',
+                          !field.value.from && 'text-muted-foreground'
+                        )}
+                      >
+                        <CalendarIcon className="mr-3 h-4 w-4 opacity-50" />
+                        {field.value?.from ? (
+                          field.value?.to ? (
+                            <>
+                              {format(field.value?.from, 'LLL dd, y')} –{' '}
+                              {format(field.value?.to, 'LLL dd, y')}
+                            </>
+                          ) : (
+                            format(field.value?.from, 'LLL dd, y')
+                          )
+                        ) : (
+                          <span>Select your dates</span>
+                        )}
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                </Popover>
+              </FormItem>
             )}
           />
         </div>
