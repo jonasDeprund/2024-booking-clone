06c17561b96392c92ed6b25ad400c4b4292a8569
Jonas Deprund <jonasdeprund@gmail.com>
2025-02-24 18:15:23 +0100

    search param wip

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -23,7 +23,28 @@ async function SearchPage({ searchParams }: Props) {
 
   console.log(results);
 
-  return <div>SearchPage</div>;
+  return (
+    <section>
+      <div className="mx-auto max-w-7xl p-6 lg:px-8">
+        <h1 className="text-4xl font-bold pb-3">Your Trip Results</h1>
+        <h2 className="pb-3">
+          Dates of trip :
+          <span className="italic ml-2">
+            {searchParams.checkin} to {searchParams.checkout}
+          </span>
+        </h2>
+        <h3 className="font-semibold text-xl">
+          {results.content.total_listings}
+        </h3>
+
+        <div className="space-y-2 mt-5">
+          {results.content.listings.map((item, i) => (
+            <div key={i}></div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default SearchPage;
